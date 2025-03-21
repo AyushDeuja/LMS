fcf1540b346227e48bf68b4ff7077f8d727698c4
AyushDeuja <ayushdeuja11@gmail.com>
2025-03-21 14:54:22 +0545

    cloudinary added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,16 @@ import { UsersModule } from './users/users.module';
 import { MembersModule } from './members/members.module';
 import { BooksModule } from './books/books.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { ClodinaryService } from './clodinary/clodinary.service';
+import { ClodinarynestService } from './generate/clodinarynest/clodinarynest.service';
+import { Cloudinary } from './cloudinary/cloudinary';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
 
 @Module({
-  imports: [UsersModule, MembersModule, BooksModule, TransactionsModule],
+  imports: [UsersModule, MembersModule, BooksModule, TransactionsModule, CloudinaryModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ClodinarynestService, Cloudinary, ClodinaryService],
 })
 export class AppModule {}
