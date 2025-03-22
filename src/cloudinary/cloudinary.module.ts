2896aafdd5c039e677dcf3698f6464b6c510d5ee
AyushDeuja <ayushdeuja11@gmail.com>
2025-03-22 18:26:25 +0545

    cloudinary setup complete

--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CloudinaryService } from './cloudinary.service';
 import { Cloudinary } from './cloudinary/cloudinary';
+import { CloudinaryProvider } from './cloudinary.provider';
 
 @Module({
-  providers: [CloudinaryService, Cloudinary]
+  providers: [CloudinaryService, Cloudinary, CloudinaryProvider],
+  exports: [CloudinaryProvider, CloudinaryService],
 })
 export class CloudinaryModule {}
