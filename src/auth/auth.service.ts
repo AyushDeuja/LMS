fce14a72cabac100b5d0382f8c42a08fdcf9f488
AyushDeuja <ayushdeuja11@gmail.com>
2025-03-28 13:34:52 +0545

    register and login auth created

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly prisma: PrismaClient,
     private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
   ) {}
   async register(registerDto: RegisterDto) {
     const user = await this.usersService.create(registerDto);
-    return user;
+    const token = await this.jwtService.signAsync(user);
+    return { token };
+  }
+  async login(loginDto: LoginDto) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        OR: [
+          {
+            email: loginDto.username,
+          },
+          {
+            mobile: loginDto.username,
+          },
+        ],
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('Unable to find user');
+    }
+    const isPasswordValid = await compare(loginDto.password, user.password);
+
+    if (!isPasswordValid) {
+      throw new NotFoundException('Invalid Credentials');
+    }
+
+    const token = await this.jwtService.signAsync(user);
+    return { token };
   }
 }
