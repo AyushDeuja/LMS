08146968fd903f7b29c622dfd620e0e4d77bc0ce
AyushDeuja <ayushdeuja11@gmail.com>
2025-03-29 10:47:02 +0545

    auth guard created

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/register.dto';
@@ -38,7 +42,7 @@ export class AuthService {
     const isPasswordValid = await compare(loginDto.password, user.password);
 
     if (!isPasswordValid) {
-      throw new NotFoundException('Invalid Credentials');
+      throw new UnauthorizedException('Invalid Credentials');
     }
 
     const token = await this.jwtService.signAsync(user);
