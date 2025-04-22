7b482be154c0d960af1dd259321147e028dfc488
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-22 13:29:14 +0545

    small change

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   app.useGlobalGuards(new AuthGuard(new JwtService(), new Reflector()));
 
   //Application starts here
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
