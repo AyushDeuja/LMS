43ba51b0231edc17b749304a97e1a7cd56e134ea
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-23 14:03:11 +0545

    form submission logic started

--- Frontend/src/pages/register.tsx
+++ Frontend/src/pages/register.tsx
@@ -4,13 +4,20 @@ import Input from "../components/Input";
 import { FormEvent } from "react";
 
 const Register = () => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const formValues = Object.fromEntries(formData.entries());
+    console.log("Form Values:", formValues);
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-indigo-700 to-purple-600">
       <div className="bg-white p-8 rounded-2xl shadow-xl w-full max-w-md">
         <h1 className="text-2xl font-bold text-center mb-6 text-gray-800">
           Register
         </h1>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={handleSubmit}>
           <Input name="name" type="text" id="name" content="Name" />
           <Input name="email" type="email" id="email" content="Email" />
           <Input name="mobile" type="tel" id="mobile" content="Mobile" />
