c67d3f398842e539dbb546cda124b0531eba1603
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-25 14:49:04 +0545

    data fetched from books api

--- Frontend/src/pages/books.tsx
+++ Frontend/src/pages/books.tsx
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
 import { axiosInstance } from "../utils/axiosInterceptor";
 
+interface Book {
+  title: string;
+  author: string;
+  id: number;
+}
 const Books = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<Book[]>([]);
   const fetchBooks = async () => {
     try {
       const response = await axiosInstance(`/books`);
@@ -18,14 +23,36 @@ const Books = () => {
   }, []);
 
   return (
-    <div className="h-full w-full flex flex-col p-8">
-      <h1 className="text-lg font-bold">Books</h1>
-      <table>
-        <thead>
-          <th>Title</th>
-          <th>Author</th>
-        </thead>
-      </table>
+    <div className="h-full w-full flex flex-col p-8 bg-gray-100">
+      <h1 className="text-2xl font-bold text-center mb-6 text-gray-800">
+        Books
+      </h1>
+      <div className="overflow-x-auto">
+        <table className="w-full border-collapse bg-white shadow-lg rounded-lg">
+          <thead>
+            <tr className="bg-indigo-600 text-white">
+              <th className="py-3 px-6 text-left">Title</th>
+              <th className="py-3 px-6 text-left">Author</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((book) => (
+              <tr
+                key={book.id}
+                className={`bg-white
+                } hover:bg-indigo-100 transition-colors`}
+              >
+                <td className="py-3 px-6 border-b border-gray-200">
+                  {book.title}
+                </td>
+                <td className="py-3 px-6 border-b border-gray-200">
+                  {book.author}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
