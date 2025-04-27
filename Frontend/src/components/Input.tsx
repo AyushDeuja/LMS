547bce2abc79aa262bceb0dd4a3eaa7718ff32f4
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-27 14:59:13 +0545

    some changes in css

--- Frontend/src/components/Input.tsx
+++ Frontend/src/components/Input.tsx
@@ -25,7 +25,7 @@ const Input = ({ label, type, id, name, required }: InputProps) => {
       <label htmlFor={name} className="block text-sm font-medium text-gray-700">
         {label}
       </label>
-      <div className="flex items-center justify-between w-full px-2 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
+      <div className="flex items-center justify-between w-full px-2 py-2 border border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
         <input
           type={inputType}
           id={id}
--- Frontend/src/layout/AppLayout.tsx
+++ Frontend/src/layout/AppLayout.tsx
@@ -8,7 +8,7 @@ const AppLayout = () => {
       <Sidebar />
       {/* Main Content */}
       <main className="w-full h-full flex justify-center items-center p-4 overflow-y-auto">
-        <div className="w-full h-full bg-slate-200 rounded-2xl shadow-lg p-4 overflow-hidden">
+        <div className="w-full h-full p-4 overflow-hidden">
           <Outlet />
         </div>
       </main>
