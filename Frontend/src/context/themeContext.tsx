e476b03ddc4ea5c11140975e914686b528a41e9b
AyushDeuja <ayushdeuja11@gmail.com>
2025-05-04 14:27:17 +0545

    usememo used

--- Frontend/src/context/themeContext.tsx
+++ Frontend/src/context/themeContext.tsx
@@ -1,11 +1,12 @@
-import { createContext, useState } from "react";
+import { createContext, useMemo, useState } from "react";
 
 //themeContext for implementing dark mode and light mode
 // context provider stores and manipulates the context data
+type Theme = "light" | "dark";
 
 interface ThemeContextValues {
-  theme?: string;
-  setTheme: (value: string) => void;
+  theme: Theme;
+  setTheme: (value: Theme) => void;
 }
 
 const ThemeContext = createContext<ThemeContextValues>({
@@ -14,11 +15,10 @@ const ThemeContext = createContext<ThemeContextValues>({
 });
 
 const ThemeProvider = ({ children }: { children: React.ReactElement }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState<Theme>("light");
+  const value = useMemo(() => ({ theme, setTheme }), [theme]);
   return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
-      {children}
-    </ThemeContext.Provider>
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
   );
 };
 
