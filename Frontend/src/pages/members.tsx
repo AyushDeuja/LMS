72b7391dd468f55f3f2d1918a41073c1a4890268
AyushDeuja <ayushdeuja11@gmail.com>
2025-05-13 13:35:55 +0545

    members not found added

--- Frontend/src/pages/members.tsx
+++ Frontend/src/pages/members.tsx
@@ -87,51 +87,59 @@ const Members = () => {
           />
         </div>
         <div className="flex-1 overflow-x-auto overflow-y-auto">
-          <table className="w-full border-collapse bg-white shadow-xl rounded-lg">
-            <thead className="sticky top-0 bg-gradient-to-l from-indigo-700 to-purple-600 text-white z-10">
-              <tr>
-                <th className="py-3 px-6 text-left">Name</th>
-                <th className="py-3 px-6 text-left">Address</th>
-                <th className="py-3 px-6 text-left">Email</th>
-                <th className="py-3 px-6 text-left">Mobile</th>
-                <th className="py-3 px-6 text-center">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredMembers.map((member) => (
-                <tr
-                  key={member.id}
-                  className={`bg-white hover:bg-indigo-100 transition-colors`}
-                >
-                  <td className="py-3 px-6 border-b border-gray-200 font-bold text-lg">
-                    {member.name}
-                  </td>
-                  <td className="py-3 px-6 border-b border-gray-200 font-semibold">
-                    {member.address}
-                  </td>
-                  <td className="py-3 px-6 border-b border-gray-200">
-                    {member.email}
-                  </td>
-                  <td className="py-3 px-6 border-b border-gray-200">
-                    {member.mobile}
-                  </td>
-
-                  <td>
-                    <div className="flex items-center justify-center gap-4">
-                      <PencilIcon
-                        className="text-blue-400 cursor-pointer"
-                        onClick={() => navigate(`/edit-member/${member.id}`)}
-                      />
-                      <Trash2Icon
-                        className="text-red-400 cursor-pointer"
-                        onClick={() => openModal(member.id as number)}
-                      />
-                    </div>
-                  </td>
+          {filteredMembers.length > 0 ? (
+            <table className="w-full border-collapse bg-white shadow-xl rounded-lg">
+              <thead className="sticky top-0 bg-gradient-to-l from-indigo-700 to-purple-600 text-white z-10">
+                <tr>
+                  <th className="py-3 px-6 text-left">Name</th>
+                  <th className="py-3 px-6 text-left">Address</th>
+                  <th className="py-3 px-6 text-left">Email</th>
+                  <th className="py-3 px-6 text-left">Mobile</th>
+                  <th className="py-3 px-6 text-center">Actions</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {filteredMembers.map((member) => (
+                  <tr
+                    key={member.id}
+                    className={`bg-white hover:bg-indigo-100 transition-colors`}
+                  >
+                    <td className="py-3 px-6 border-b border-gray-200 font-bold text-lg">
+                      {member.name}
+                    </td>
+                    <td className="py-3 px-6 border-b border-gray-200 font-semibold">
+                      {member.address}
+                    </td>
+                    <td className="py-3 px-6 border-b border-gray-200">
+                      {member.email}
+                    </td>
+                    <td className="py-3 px-6 border-b border-gray-200">
+                      {member.mobile}
+                    </td>
+
+                    <td>
+                      <div className="flex items-center justify-center gap-4">
+                        <PencilIcon
+                          className="text-blue-400 cursor-pointer"
+                          onClick={() => navigate(`/edit-member/${member.id}`)}
+                        />
+                        <Trash2Icon
+                          className="text-red-400 cursor-pointer"
+                          onClick={() => openModal(member.id as number)}
+                        />
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <div className="flex items-center justify-center h-[80vh]">
+              <p className="text-red-500 text-3xl font-bold">
+                No members found.
+              </p>
+            </div>
+          )}
         </div>
         <Modal
           isModalOpen={isModalOpen}
