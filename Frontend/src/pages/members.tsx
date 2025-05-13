976b1b19e6b3cfd9ed2622b7af5a63f534d979f4
AyushDeuja <ayushdeuja11@gmail.com>
2025-05-13 13:24:38 +0545

    input field created

--- Frontend/src/pages/members.tsx
+++ Frontend/src/pages/members.tsx
@@ -63,12 +63,6 @@ const Members = () => {
                 }
               }}
             />
-            <Button
-              label="Search"
-              type="button"
-              className="rounded-l-none ml-0 !w-20"
-              onClick={handleSearch}
-            />
           </div>
           <Button
             label="Add Member"
